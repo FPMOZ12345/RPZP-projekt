5b7b3b39e5563dbd55cecd6073e931741ec14310
FPMOZ12345 <48982605+FPMOZ12345@users.noreply.github.com>
2019-08-14 20:10:32 +0200

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlite3 import *
-
-veza = connect("kalendar_događaja.db")
-
-k = veza.cursor()
-
-k.execute("CREATE TABLE IF NOT EXISTS događaji (naziv_događaja TEXT, datum_događaja TEXT, opis_događaja TEXT)")
-
-veza.close()
